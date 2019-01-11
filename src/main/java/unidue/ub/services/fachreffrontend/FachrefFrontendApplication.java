5922207816ada7c2f5bb74221eab12d2e1f53cae
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-01-11 13:20:33 +0100

    update to OpenJDK 11

--- src/main/java/unidue/ub/services/fachreffrontend/FachrefFrontendApplication.java
+++ src/main/java/unidue/ub/services/fachreffrontend/FachrefFrontendApplication.java
@@ -2,27 +2,23 @@ package unidue.ub.services.fachreffrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class FachrefFrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FachrefFrontendApplication.class, args);
 	}
 
-
 	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		@Override
@@ -30,8 +26,8 @@ public class FachrefFrontendApplication {
 			http.httpBasic()
 					.and()
 					.authorizeRequests().anyRequest().authenticated()
-			.and()
-					.csrf().disable();
+					.and()
+					.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		}
 	}
 }
